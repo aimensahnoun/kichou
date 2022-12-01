804fa0b39043876ea851bdd4b3ae6f7867ef2bbb
Aimen Sahnoun <aimensahnoun@outlook.com>
2022-12-02 00:31:20 +0300

    Make landing page responsive

---
+++ hardhat/contracts/MarketItem.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MarketItem is ERC721, ERC721URIStorage {
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
+
+    
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+}
--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -17,18 +17,20 @@ export default function Home() {
       </nav>
 
 
-      <section className='flex w-full h-full gap-x-[5rem] justify-center items-center'>
+      <section className='flex flex-col md:flex-row w-full h-full gap-y-12 gap-x-[5rem] justify-center items-center'>
         <div className='flex flex-col'>
 
-          <span className='font-bold text-4xl'>Kichō</span>
-          <pre>
-            A Maketplace of
+          <span className='font-bold text-5xl'>Kichō</span>
+          <pre className='text-kichou-gray duration-300 hover:text-white'>
+            A Marketplace of
             Wonders
           </pre>
         </div>
 
         <div className='flex rounded-lg relative'>
-          <Image src="/hero.svg" className='bg-inherit rounded-lg bounce z-0 duration-300 hover:scale-105' alt='Marketplace' width={400} height={400} />
+          <Image src="/hero.svg" className='hidden md:block bg-inherit rounded-lg bounce z-0 duration-300 hover:scale-105' alt='Marketplace' width={400} height={400} />
+
+          <Image src="/hero.svg" className='md:hidden bg-inherit rounded-lg bounce z-0 duration-300 hover:scale-105' alt='Marketplace' width={300} height={300} />
           {
             [...Array(15)].map((_, i) => {
               return <div key={i} className="firefly z-10" />
