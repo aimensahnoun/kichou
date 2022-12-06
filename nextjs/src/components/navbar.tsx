9603513aebf22b338d46d68ec3a351b7a3ff60f2
Aimen Sahnoun <aimensahnoun@outlook.com>
2022-12-06 21:04:25 +0300

    Navbar responsiveness

--- nextjs/src/components/navbar.tsx
+++ nextjs/src/components/navbar.tsx
@@ -24,7 +24,7 @@ const NavBar = () => {
     const navbarRef = useRef<HTMLDivElement>(null)
 
     // Wagmi 
-    const { isConnected , address } = useAccount()
+    const { isConnected, address } = useAccount()
 
     // UseEffects
     useEffect(() => {
@@ -52,29 +52,39 @@ const NavBar = () => {
 
     return <nav ref={navbarRef} className='flex w-full items-center justify-between  p-4 border-b-[1px] border-b-kichou-gray'>
 
-        <div className="flex gap-x-7 items-center">
+        <div className="flex gap-x-4  md:gap-x-7 items-center">
             <Link href="/">
                 <div className='flex items-center gap-x-4'>
                     <Image src="/logo.svg" alt='Kichō Logo' width={70} height={70} />
-                    <div className='flex flex-col'>
+                    <div className='flex-col hidden md:flex'>
                         <span className='font-bold text-3xl'>Kichō</span>
                         <span className='text-kichou-gray font-bold text-xl'>貴重</span>
                     </div>
 
                 </div>
             </Link>
-            <div className="flex items-center gap-x-4">
+            <div className="flex items-center  gap-x-4">
                 {
                     tabs.map((tab) => {
                         return <Link key={tab.href} href={tab.href}
-                            className={"text-kichou-gray duration-200 hover:text-white text-shadow"} >
+                            className={"text-sm md:text-base text-kichou-gray duration-200 hover:text-white text-shadow"} >
                             {tab.name}
                         </Link>
                     })
                 }
             </div>
         </div>
-        <ConnectButton label='Get Started' />
+        <ConnectButton showBalance={{
+            smallScreen: false,
+            largeScreen: true,
+
+        }}
+            accountStatus={{
+                smallScreen: "avatar",
+                largeScreen: "full",
+            }}
+
+            label='Get Started' />
     </nav>
 }
 
